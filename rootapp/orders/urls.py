81ff8d16bd686145096e54335b4d990e7b43f742
RadoTk <hei.rado.3@gmail.com>
2025-10-27 09:50:42 +0300

    fix(orders/stripe): fix multiple issues with order creation, payment, and webhook handling

--- rootapp/orders/urls.py
+++ rootapp/orders/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('create/', order_create, name="order_create"),
     path('thanks/', views.order_success_view, name='order_thanks'),
     path("refresh-badge/", views.get_new_orders_count, name="refresh_order_badge"),
-    path('stripe/webhook/', stripe_webhook, name='stripe_webhook'),
+    path('stripe/webhook/', stripe_webhook, name='stripe-webhook'),
 ]
